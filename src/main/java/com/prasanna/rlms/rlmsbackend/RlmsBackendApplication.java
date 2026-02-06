eb4fa1374795854a81023ae8784f8c7a4a0d66e5
PrasannaLanka <prasu1760@gmail.com>
2026-02-06 06:35:24 +0530

    backend set up is done

---
+++ src/main/java/com/prasanna/rlms/rlmsbackend/RlmsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.prasanna.rlms.rlmsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RlmsBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RlmsBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/prasanna/rlms/rlmsbackend/RlmsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prasanna.rlms.rlmsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RlmsBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
